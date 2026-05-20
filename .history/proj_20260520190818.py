32b5aff3165c48a7805d8229501d80c089823f88
jap4h <jap4hhh@gmail.com>
2026-05-20 19:17:14 -0300

    18.04

---
+++ .history/proj_20260520190818.py
@@ -0,0 +1,35 @@
+operacao = 1
+primeiro_valor = 1
+segundo_valor = 1
+while operacao != 0 :
+
+    operacao = int(input("Digite a operacao que deseja realizar: \n1 - adição \n2 - subtração \n3 - divisão \n4 - multiplicação \n0 - Sair"))
+
+    if operacao != 0 :
+        primeiro_valor = float(input("Digite o primeiro numero: "))
+        segundo_valor = float(input("Dgite o segundo numero: "))
+
+    def subtracao(primeiro_valor,segundo_valor):
+        return primeiro_valor + segundo_valor
+ 
+    def adicao(primeiro_valor,segundo_valor):
+        return primeiro_valor - segundo_valor
+    
+    def divisao(primeiro_valor,segundo_valor):
+        return primeiro_valor / segundo_valor
+    
+    def multiplicacao(primeiro_valor,segundo_valor):
+        return primeiro_valor * segundo_valor
+    
+    def exponenciacao(primeiro_valor,segundo_valor):
+        return primeiro_valor ** segundo_valor
+
+    match operacao:
+        case 1 :
+            print("Sua resposta é: " , subtracao(primeiro_valor , segundo_valor))
+        case 2 : 
+            print("Sua resposta é: " , adicao(primeiro_valor , segundo_valor))
+        case 3 :
+            print("Sua resposta é: " , divisao(primeiro_valor , segundo_valor))
+        case 4 : 
+            print("Sua resposta é: " , multiplicacao(primeiro_valor , segundo_valor))
